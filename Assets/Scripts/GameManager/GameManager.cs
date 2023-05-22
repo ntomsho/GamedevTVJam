bfe609f7cc805436db7d2e2f18cee9b77a12dc5a
Nick Tomsho <46005988+ntomsho@users.noreply.github.com>
2023-05-22 00:17:58 -0400

    Implement Harmony UI

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public struct HarmonyPair
+{
+    public int natureHarmony;
+    public int techHarmony;
+    public HarmonyPair(int natureHarmony, int techHarmony)
+    {
+        this.natureHarmony = natureHarmony;
+        this.techHarmony = techHarmony;
+    }
+}
 
 public class GameManager : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public event EventHandler<HarmonyPair> OnHarmonyChanged;
+    public int natureHarmony;
+    public int techHarmony;
+
     //public GameObject crosshair;
+    
     // Start is called before the first frame update
     void Start()
     {
 
+    }
+
+    public void GainHarmony(int natureHarmonyToAdd, int techHarmonyToAdd)
+    {
+        natureHarmony += natureHarmonyToAdd;
+        techHarmony += techHarmonyToAdd;
+
+        OnHarmonyChanged?.Invoke(this, new HarmonyPair(natureHarmony, techHarmony));
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/HarmonyUI.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HarmonyUI : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager;
+    [SerializeField] TextMeshProUGUI natureHarmonyCounter;
+    [SerializeField] TextMeshProUGUI techHarmonyCounter;
+    [SerializeField] TextMeshProUGUI totalHarmonyCounter;
+
+    int displayNatureHarmony = 0;
+    int displayTechHarmony = 0;
+
+    int realNatureHarmony;
+    int realTechHarmony;
+
+    [SerializeField] float displayTextUpdatePerSecond = 20f;
+
+    void Start()
+    {
+        gameManager.OnHarmonyChanged += UpdateHarmonyValues;
+    }
+
+    void UpdateHarmonyValues(object sender, HarmonyPair harmonyPair)
+    {
+        if (harmonyPair.natureHarmony == 0 && harmonyPair.techHarmony == 0) return;
+        realNatureHarmony = harmonyPair.natureHarmony;
+        realTechHarmony = harmonyPair.techHarmony;
+    }
+
+    void UpdateDisplayValues()
+    {
+        if (displayNatureHarmony < realNatureHarmony)
+        {
+            displayNatureHarmony = (int)Mathf.Floor(Mathf.Min(displayNatureHarmony + ((byte)(Time.deltaTime * displayTextUpdatePerSecond)), realNatureHarmony));
+        }
+        if (displayTechHarmony < realTechHarmony)
+        {
+            displayTechHarmony = (int)Mathf.Floor(Mathf.Min(displayTechHarmony + ((byte)(Time.deltaTime * displayTextUpdatePerSecond)), realTechHarmony));
+        }
+
+        UpdateUIText();
+    }
+
+    void UpdateUIText()
+    {
+        natureHarmonyCounter.text = displayNatureHarmony.ToString();
+        techHarmonyCounter.text = displayTechHarmony.ToString();
+        totalHarmonyCounter.text = Mathf.Min(displayNatureHarmony, displayTechHarmony).ToString();
+    }
+
+    void Update()
+    {
+        if (displayNatureHarmony == realNatureHarmony && displayTechHarmony == realTechHarmony) return;
+        UpdateDisplayValues();
+    }
+}
